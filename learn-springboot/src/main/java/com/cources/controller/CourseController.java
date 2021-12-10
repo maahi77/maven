c78f95208f7686309cfcf96e1de9ba21a6e61604
maahi77 <86926070+maahi77@users.noreply.github.com>
2021-12-10 11:28:23 +0530

    commit

---
+++ learn-springboot/src/main/java/com/cources/controller/CourseController.java
@@ -0,0 +1,19 @@
+package com.cources.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cources.bean.Course;
+
+@RestController
+public class CourseController {
+	
+	@GetMapping("/courses")
+	public List<Course> getAllCourses(){
+		return Arrays.asList(new Course(1,"mahi","in28min"));
+	}
+
+}
---
+++ learn-springboot/src/test/java/com/in28min/learnspringboot/LearnSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28min.learnspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
