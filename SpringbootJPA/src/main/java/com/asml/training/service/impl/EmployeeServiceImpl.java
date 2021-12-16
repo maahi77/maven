5c2e4b6e9e353ccf6825a0bef58685b90bf7d203
maahi77 <86926070+maahi77@users.noreply.github.com>
2021-12-16 11:16:45 +0530

    Commit1

---
+++ SpringbootJPA/src/main/java/com/asml/training/service/impl/EmployeeServiceImpl.java
@@ -0,0 +1,45 @@
+package com.asml.training.service.impl;
+/* 
+ * 
+ * @author mahendra
+ * @since 2021-12-10
+ *
+ */
+
+
+import com.asml.training.entity.Employee;
+import com.asml.training.repository.impl.EmployeeDaoImpl;
+import com.asml.training.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmployeeServiceImpl  implements EmployeeService {
+    @Autowired
+    EmployeeDaoImpl employeeDao;
+
+    @Override
+    public void add(Employee e) {
+        employeeDao.add(e);
+    }
+
+    @Override
+    public void update(Employee e) {
+        employeeDao.update(e);
+    }
+
+    @Override
+    public List<Employee> get() {
+        return employeeDao.findAll();
+    }
+    @Override
+    public List<Employee> findByname(String name) {
+    	return employeeDao.getByName(name);
+    }
+    @Override
+    public List<Employee> findByNameAndEmail(String name,String email){
+    	return employeeDao.getByNameAndEmail( name, email);
+    }
+}
